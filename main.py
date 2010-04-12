4cf70599451f539390b76f99cb0048488d2a9b2f
Justin White <just6979@gmail.com>
2010-04-11 23:48:51 -0400

    Version 4 deployed. Cleaned up main.py. Removed journal and picture files. Started Edit controls

--- main.py
+++ main.py
@@ -8,109 +8,61 @@ import wsgiref
 import cgi
 import Cookie
 
+from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-import journal
-
+class Page(db.Model):
+	title = db.StringProperty(required=True)
+	author = db.UserProperty(required=True)
+	type = db.StringProperty(required=True)
+	content = db.TextProperty(required=True)
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
-		req = self.request
-		env = req.environ
-		hdrs = req.headers
-		res = self.response
-
-		base_dir = os.path.dirname(__file__)
-		template_dir = os.path.join(base_dir, 'templates')
-		content_dir = os.path.join(base_dir, 'content')
-
-		# datestamp for HTTP headers: LastModified, Expires
-		http_date_stamp = '%a, %d %b %Y %H:%M:%S GMT'
-		# datestamp for cookies
-		cookie_date_stamp = '%a, %d-%b-%Y %H:%M:%S GMT'
-
 		user = users.get_current_user()
 		admin = users.is_current_user_admin()
 
-		now = time.time()
-		res.headers['Expires'] = time.strftime(http_date_stamp, time.gmtime(now))
+		form = cgi.FieldStorage(environ=self.request.environ)
 
-		# save the referer for possible redirects
-		referer = env.get('HTTP_REFERER', '')
-
-		# parse CGI form data
-		form = cgi.FieldStorage(environ=env)
 		op = form.getfirst('op', 'display').lower()
 
-		if op == 'dump':
-			page = form.getfirst('p', os.path.basename(__file__))
-			page_file = os.path.join(base_dir, page)
-			try:
-				filedata = file(page_file, 'r')
-			except IOError:
-				page_file = os.path.join(base_dir, os.path.basename(__file__))
-				filedata = file(page_file, 'r')
-			else:
-				pass
-		else:
+		edit = ''
+		if op == 'edit':
+			edit = 'edit'
+			op = 'display'
+
+		if op == 'display':
 			page = form.getfirst('p', 'home')
-			if page == 'journal':
-				page_file = os.path.join(base_dir, 'journal.py')
-				j = journal.Journal(
-					form,
-					user
-				)
-				content = j.dispatch()
-			else:
-				# try to open the requested page .htf file
-				try:
-					page_file = os.path.join(content_dir, page + '.htf')
-					content = file(page_file, 'r')
-				# if not, use tinfoil.htf. if it's not there we got bigger probs
-				except IOError:
-					page = 'home'
-					page_file = os.path.join(content_dir, page + '.htf')
-					content = file(page_file, 'r')
+			content_dir = 'content'
+			try:
+				page_file = os.path.join(content_dir, page + '.htf')
+				content = file(page_file, 'r')
+			except IOError:
+				page = 'home'
+				page_file = os.path.join(content_dir, page + '.htf')
+				content = file(page_file, 'r')
+
 			title = page.capitalize()
 
-		# MS Internet Explorer (<= 7) doesn't understand application/xhtml+xml
-		# If the request came from MSIE (<= 7), then use text/html instead
-		agent = env.get('HTTP_USER_AGENT', '')
-		if 'MSIE' in agent:
-			res.headers['Content-type'] = 'text/html; charset=utf-8'
-		else:
-			res.headers['Content-type'] = 'application/xhtml+xml; charset=utf-8'
+			path = 'templates/main.html'
+			values = {
+				'title': title,
+				'page': page,
+				'content': content.readlines(),
+				'user': user,
+				'admin': admin,
+				'edit': edit,
+				'datetime': None,
+			}
 
-		# get file modification times
-		mod_time = os.stat(page_file)[8]
-		# and format a nice HTTP style datestamp
-		pretty_mod_time = time.strftime(http_date_stamp, time.gmtime(mod_time))
-		# and send it to the client
-		res.headers['LastModified'] = pretty_mod_time
-
-		# load the template
-		template_file = 'main.html'
-		path = os.path.join(template_dir, template_file)
-		values = {
-			'title': title,
-			'user': user,
-			'admin': admin,
-			'page': page,
-			'content': content,
-			'page_file': os.path.basename(page_file),
-			'mod_time': pretty_mod_time,
-		}
-		# show it off!
-		res.out.write(template.render(path, values))
-
+			self.response.out.write(template.render(path, values))
 
 class LoginPage(webapp.RequestHandler):
 	def get(self):
 		self.redirect(users.create_login_url(self.request.referrer))
-
 
 class LogoutPage(webapp.RequestHandler):
 	def get(self):
@@ -120,14 +72,12 @@ class MercurialPage(webapp.RequestHandler):
 	def get(self):
 		self.redirect('http://just6979.hg.sourceforge.net/hgweb/just6979/just6979/')
 
-
 application = webapp.WSGIApplication([
 	('/', MainPage),
 	('/login', LoginPage),
 	('/logout', LogoutPage),
 	('/hg', MercurialPage),
 ], debug=True)
-
 
 def main():
     run_wsgi_app(application)
