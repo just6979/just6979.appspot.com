5044faa70e559d77bef1da7da0fe5600f0bd23e2
Justin White <jw@justinwhite.net>
2017-04-13 22:04:38 -0400

    Update to App Engine Python 2.7

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 # -*- coding: utf-8 -*-
 import os
-import sys
-import time
-import traceback
-import wsgiref
-
-import cgi
-import Cookie
-
-from google.appengine.ext import db
+import webapp2
 from google.appengine.api import users
+from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
-from google.appengine.ext.webapp.util import run_wsgi_app
 
 class Page(db.Model):
 	datetime = db.DateTimeProperty(required=True)
@@ -84,8 +76,4 @@ application = webapp.WSGIApplication([
 	('/hg', MercurialPage),
 ], debug=True)
 
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
+app = webapp2.WSGIApplication([('/', MainPage)])
