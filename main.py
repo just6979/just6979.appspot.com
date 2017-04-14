2ef740b221ac47d0bc3d22542047aaac075bb9cb
Justin White <jw@justinwhite.net>
2017-04-13 22:10:43 -0400

    Code formatting.

--- main.py
+++ main.py
@@ -6,74 +6,83 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
+
 class Page(db.Model):
-	datetime = db.DateTimeProperty(required=True)
-	title = db.StringProperty(required=True)
-	author = db.UserProperty(required=True)
-	content = db.TextProperty(required=True)
-	tags = db.StringProperty()
+    datetime = db.DateTimeProperty(required=True)
+    title = db.StringProperty(required=True)
+    author = db.UserProperty(required=True)
+    content = db.TextProperty(required=True)
+    tags = db.StringProperty()
 
-def getPageData(page):
-		content_dir = 'content'
-		try:
-			page_file = os.path.join(content_dir, page.lower() + '.htf')
-			data = file(page_file, 'r')
-		except IOError:
-			page = 'home'
-			page_file = os.path.join(content_dir, page.lower() + '.htf')
-			data = file(page_file, 'r')
 
-		content = ''
-		for line in data:
-			content += line
+def get_page_data(page):
+    content_dir = 'content'
+    try:
+        page_file = os.path.join(content_dir, page.lower() + '.htf')
+        data = file(page_file, 'r')
+    except IOError:
+        page = 'home'
+        page_file = os.path.join(content_dir, page.lower() + '.htf')
+        data = file(page_file, 'r')
 
-		return content
+    content = ''
+    for line in data:
+        content += line
+
+    return content
+
 
 class SinglePage(webapp.RequestHandler):
-	def get(self, page):
-		user = users.get_current_user()
-		admin = users.is_current_user_admin()
+    def get(self, page):
+        user = users.get_current_user()
+        admin = users.is_current_user_admin()
 
-		title = page.capitalize()
+        title = page.capitalize()
 
-		path = 'html/main.html'
-		values = {
-			'title': title,
-			'page': page,
-			'content': getPageData(page),
-			'user': user,
-			'admin': admin,
-		}
+        path = 'html/main.html'
+        values = {
+            'title': title,
+            'page': page,
+            'content': get_page_data(page),
+            'user': user,
+            'admin': admin,
+        }
 
-		self.response.out.write(template.render(path, values))
+        self.response.out.write(template.render(path, values))
+
 
 class UpdatePage(webapp.RequestHandler):
-	def post(self, page, form):
-		pass
+    def post(self, page, form):
+        pass
+
 
 class LoginPage(webapp.RequestHandler):
-	def get(self):
-		self.redirect(users.create_login_url(self.request.referrer))
+    def get(self):
+        self.redirect(users.create_login_url(self.request.referrer))
+
 
 class LogoutPage(webapp.RequestHandler):
-	def get(self):
-		self.redirect(users.create_logout_url(self.request.referrer))
+    def get(self):
+        self.redirect(users.create_logout_url(self.request.referrer))
+
 
 class MainPage(webapp.RequestHandler):
-	def get(self):
-		self.redirect('/page/home')
+    def get(self):
+        self.redirect('/page/home')
+
 
 class MercurialPage(webapp.RequestHandler):
-	def get(self):
-		self.redirect('http://bitbucket.org/just6979/just6979')
+    def get(self):
+        self.redirect('http://bitbucket.org/just6979/just6979')
+
 
 application = webapp.WSGIApplication([
-	('/', MainPage),
-	(r'/page/(.*)', SinglePage),
-	(r'/update/(.*)', UpdatePage),
-	('/login', LoginPage),
-	('/logout', LogoutPage),
-	('/hg', MercurialPage),
+    ('/', MainPage),
+    (r'/page/(.*)', SinglePage),
+    (r'/update/(.*)', UpdatePage),
+    ('/login', LoginPage),
+    ('/logout', LogoutPage),
+    ('/hg', MercurialPage),
 ], debug=True)
 
 app = webapp2.WSGIApplication([('/', MainPage)])
